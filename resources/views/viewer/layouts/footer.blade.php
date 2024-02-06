0e8333144841a4e7cd220800fb23aa540c085b80
qhlinh <qhlinh86@gmail.com>
2024-02-06 10:22:59 +0700

    06/02

--- resources/views/viewer/layouts/footer.blade.php
+++ resources/views/viewer/layouts/footer.blade.php
@@ -4,8 +4,8 @@
             <div class="column one">
                 <!-- Copyrights -->
                 <div class="copyright">
-                    Copyright © Starpol Trading. All rights reserved.					</div>
-                <ul class="social"><li class="linkedin"><a  href="https://www.linkedin.com/company/starpol-trading-llc/" title="LinkedIn"><i class="icon-linkedin"></i></a></li></ul>
+                    Copyright © romaplas.com Trading. All rights reserved.					</div>
+                <ul class="social"><li class="linkedin"><a  href="https://www.linkedin.com/" title="LinkedIn"><i class="icon-linkedin"></i></a></li></ul>
             </div>
         </div>
     </div>
